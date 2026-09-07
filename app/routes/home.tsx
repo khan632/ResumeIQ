f8586f826fc0358968b4e3064356a96c52d81066
khan632 <rmustafa632@gmail.com>
2026-09-07 20:27:53 +0530

    Added aunthetication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/component/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "~/constant";
 import { ResumeCard } from "~/component/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate, type NavigateFunction } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +14,12 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {isLoading, auth} = usePuterStore();
+    const navigate: NavigateFunction = useNavigate();
+
+    useEffect(() => {
+        if(auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated, ])
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
     <section className="main-section">
